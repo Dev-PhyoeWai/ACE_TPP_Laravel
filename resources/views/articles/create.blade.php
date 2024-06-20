05bea29aad6b03263e89d9b6217d9576f4bef962
dev-phyoewai <phyoewai2311@gmail.com>
2024-06-20 10:48:36 +0630

    Update middlewaregit status

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,32 +1,29 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Articles</title>
-</head>
-<body>
-    <h1>Create Articles</h1>
 
+@extends('layouts.app')
+@section('content')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <div class="container">
-    {{--    @method('PUT')    --}}
+        <h1 class="text-center mt-4 mb-4">Create Articles</h1>
+        {{--    @method('PUT')    --}}
+        <div class="col-6">
+            <a href="{{route('articles.index')}}" class="btn btn-sm btn-dark text-white"><i class="fa-solid fa-arrow-left-long"></i>back</a>
+        </div>
         <form method="post" action="{{ route('articles.store') }}">
             @csrf
-            <div class="row">
-                <div class="col">
+            <div class="col-md-6 offset-md-3 my-5">
+                <div class="my-4">
                     <input type="text" name="name" class="form-control" placeholder="name">
-                </div><br>
-                <div class="col">
+                </div>
+                <div class="my-4">
                     <input type="number" name="age" class="form-control" placeholder="age">
-                </div><br>
-                <div class="col">
+                </div>
+                <div class="my-4">
                     <input type="text" name="city" class="form-control" placeholder="city">
-                </div><br>
-                <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+                <button type="submit" class="btn btn-info btn-sm text-white">Create</button>
             </div>
         </form>
     </div>
-</body>
-</html>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,26 @@ Route::get('/', function () {
 //    return view('index');
 //});
 
-Route::get('category', [CategoryController::class,'index'])->name('categoryIndex');
+Route::get('category', [CategoryController::class,'index'])->name('categoryIndex')->middleware('auth');
 //Route::get('result', [CategoryController::class,'result']);
-Route::get('product', [ProductController::class,'index'])->name('productIndex');
+Route::get('product', [ProductController::class,'index'])->name('productIndex')->middleware('auth');
 
-Route::get('/category/create', [CategoryController::class,'create'])->name('categoryCreate');
-Route::post('/category/store', [CategoryController::class,'store'])->name('categoryStore');
+Route::get('/category/create', [CategoryController::class,'create'])->name('categoryCreate')->middleware('auth');
+Route::post('/category/store', [CategoryController::class,'store'])->name('categoryStore')->middleware('auth');
 
-Route::get('/category/{id}', [CategoryController::class,'edit'])->name('categoryEdit');
-Route::post('/category/update/{id}', [CategoryController::class,'update'])->name('categoryUpdate');
+Route::get('/category/{id}', [CategoryController::class,'edit'])->name('categoryEdit')->middleware('auth');
+Route::post('/category/update/{id}', [CategoryController::class,'update'])->name('categoryUpdate')->middleware('auth');
 
-Route::get('/product/create', [ProductController::class,'create'])->name('productCreate');
-Route::post('/product/store', [ProductController::class,'store'])->name('productStore');
+Route::get('/product/create', [ProductController::class,'create'])->name('productCreate')->middleware('auth');
+Route::post('/product/store', [ProductController::class,'store'])->name('productStore')->middleware('auth');
 
-Route::get('/product/{id}', [ProductController::class,'edit'])->name('productEdit');
-Route::post('/product/update/{id}', [ProductController::class,'update'])->name('productUpdate');
+Route::get('/product/{id}', [ProductController::class,'edit'])->name('productEdit')->middleware('auth');
+Route::post('/product/update/{id}', [ProductController::class,'update'])->name('productUpdate')->middleware('auth');
 
-Route::post('/category/{deldata}', [CategoryController::class,'destroy'])->name('categoryDelete');
-Route::post('/product/{productdel}', [ProductController::class,'destroy'])->name('productDelete');
+Route::post('/category/{deldata}', [CategoryController::class,'destroy'])->name('categoryDelete')->middleware('auth');
+Route::post('/product/{productdel}', [ProductController::class,'destroy'])->name('productDelete')->middleware('auth');
 
-Route::resource('articles', ArticleController::class);
+Route::resource('articles', ArticleController::class)->middleware('auth')->middleware('auth');
 
 Auth::routes();
 
